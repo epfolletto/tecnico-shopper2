97bcde82b8d8d4d40538cd64d6624b7d5920dd55
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2022-10-13 17:04:20 -0300

    alteração base url

--- frontend/src/constants/BASE_URL.js
+++ frontend/src/constants/BASE_URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL= "http://localhost:3003"
-// export const BASE_URL= "https://54.175.59.211:3003"
+export const BASE_URL= "http://ec2-54-84-62-138.compute-1.amazonaws.com:3003"
+// export const BASE_URL= "http://localhost:3003"
